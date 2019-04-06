f0661c9a71cd71b8c13411e4d9c6f5fad58e78a3
keinuma <nununu.mono@gmail.com>
2019-04-06 22:15:23 +0900

    Initial commit

---
+++ 1_4_hello/src/HelloHTML.elm
@@ -0,0 +1,7 @@
+module HelloHTML exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+main =
+    a [ href "https://elm-lang.org" ] [ text "Elm" ]
---
+++ 2_5_prepare/src/Main.elm
@@ -0,0 +1,7 @@
+module Main exposing (add, output)
+
+output =
+    "1 + 1 = " ++ String.fromInt (add 1 1)
+
+add a b =
+    a + b
